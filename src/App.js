516bf6a2b116caa7d59ed5aef50d555d8dc01c3e
avc0021 <avc0021@gmail.com>
2022-06-16 20:25:31 -0500

    syntax error in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './assets/images/BabyShowerlogo.png';
+import { Container } from '@chakra-ui/react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+    <Container className="App">
+
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
+
+
+    </Container>
     </div>
   );
 }
